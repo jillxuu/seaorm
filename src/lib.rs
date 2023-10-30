8446e80d04905899ea291816cf0ecccea3220c34
jillxuu <jill@aptoslabs.com>
2023-10-30 16:52:08 -0700

    adjust to sqlx postgres

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 use migration::{DbErr, Migrator, MigratorTrait};
-use sea_orm::{Database, DbConn};
+use sea_orm::{Database, DbConn, DatabaseConnection};
 
 pub async fn establish_connection() -> Result<DbConn, DbErr> {
     let database_url = std::env::var("DATABASE_URL").unwrap();
-    let db = Database::connect(&database_url)
+    let db: DatabaseConnection = Database::connect(&database_url)
         .await
         .expect("Failed to setup the database");
     Migrator::up(&db, None)
